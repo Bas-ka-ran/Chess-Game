e0b252cba30e6e88644197a540b08703c7fa0d46
Bas-ka-ran <116240873+Bas-ka-ran@users.noreply.github.com>
2026-03-13 10:23:53 +0000

    Saved

--- ChessGame/src/main/java/chess/gui/PieceRenderer.java
+++ ChessGame/src/main/java/chess/gui/PieceRenderer.java
@@ -1,4 +1,4 @@
-package chess.gui;
+//package chess.gui;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
---
+++ ChessGame/src/main/java/chess/shared/MessageType.java
@@ -0,0 +1,12 @@
+package chess.shared;
+
+public enum MessageType {
+    ASSIGN_COLOR,
+    GAME_START,
+    MOVE,
+    BOARD_UPDATE,
+    CLOCK_UPDATE,
+    GAME_OVER,
+    INVALID_MOVE,
+    OPPONENT_DISCONNECTED
+}
